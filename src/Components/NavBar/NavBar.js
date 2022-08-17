2d1092c3ff8638f36a13d72fa4c1b5b8f64b9e7e
Luciano Pardo <pardobio@gmail.com>
2022-08-17 10:50:10 -0300

    2nd challenge ready

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,17 +1,20 @@
 import './NavBar.css';
 import Logo from '../../Imgs/THS_logo.png'
 import {ReactComponent as CartIcon} from '../../Imgs/Icons/cart_icon.svg'
+import Container from 'react-bootstrap/Container';
 
 const NavBar = () => {
     return(
         <div className= 'NavBar'>
-            <img className= 'logo' src={Logo} alt=''/>
+            <img className= 'logo' src={Logo} alt='/'/>
             <div className= 'linksCont'>
-                <div className='navItem'>HOME</div>
-                <div className='navItem'>PRODUCTS</div>
-                <div className='navItem'>CONTACT</div>
-                <div><CartIcon className='cartIcon navItem'/></div>
-                <div className='navItem'>CART</div>
+                <a className='navItem' href='/'>HOME</a>
+                <a className='navItem' href='/'>PRODUCTS</a>
+                <a className='navItem' href='/'>CONTACT</a>
+                <div className='cartCont'>
+                    <div><CartIcon className='cartIcon navItem'/></div>
+                    <a className='navItem' href='/'>CART</a>
+                </div>                
             </div>
         </div>
     );
