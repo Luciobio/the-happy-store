84ca461299e2127749c4b409ab49369193f45551
Luciano Pardo <pardobio@gmail.com>
2022-09-04 19:36:49 -0300

    desafío 5 corregido

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -1,21 +1,7 @@
-import { useEffect, useState } from "react";
-import {getProducts} from "../../asyncMock";
 import Item from '../Item/Item'
 import './ItemList.css'
 
-const ItemList = () => {
-    const [items, setItems] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-
-    useEffect(()=>{
-        getProducts()
-        .then((res) => {
-            setItems(res);
-        })
-        .finally(() => setLoading(false)); 
-    },[]);
-
+const ItemList = ({items, loading}) => {
     const showItems = items.map(prod => <Item key= {prod.id} prod= {prod}/>)
 
     const isLoading = loading? <h2>Loading...</h2> : (
--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,23 @@
 import './ItemListContainer.css';
+import { useEffect, useState } from "react";
 import ItemList from '../ItemList/ItemList';
+import {getProducts} from "../../asyncMock";
 
 const ItemListContainer = () => {
+    const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        getProducts()
+        .then((res) => {
+            setItems(res);
+        })
+        .finally(() => setLoading(false)); 
+    },[]);
+
     return (
         <div className='ItemListContainer'>
-            <ItemList/>
+            <ItemList items= {items} loading= {loading}/>
         </div>
     );
 };
